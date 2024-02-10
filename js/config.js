f4739749e385c7ccdbffc8f5ca31894012e14826
saikumarsagar <102918746+saikumarsagar@users.noreply.github.com>
2024-02-10 21:52:54 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ZTnkM6DLm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4ajon8i75e6ns0ipiqsj83pgtn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-1
+        userPoolId: 'ap-south-1_gpeuibfOh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1fo27h1eudb4gfq03url48cjtf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: 'https://ugz5o5nmo7.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
