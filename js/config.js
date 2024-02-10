0bae55018b36858ea13aa297f547f5c5b7ba487d
saikumarsagar <102918746+saikumarsagar@users.noreply.github.com>
2024-02-10 23:21:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://i48pavf0gf.execute-api.ap-south-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
